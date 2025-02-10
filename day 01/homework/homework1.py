f8b528bc7a8635372c963b547750a1591b64bdc5
Xurtsi <mariamxurcilava363@gmail.com>
2025-02-10 07:17:31 -0800

    commit

---
+++ day 01/homework/homework1.py
@@ -0,0 +1,35 @@
+c#name="mari"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "mari" არის ცვლადისთვის მნიშვნელობა
+
+surname ="xurtsilava"
+
+#print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name ="mari" #ეს არის  str (string) ტიპის ცვლადი
+age= 13 #ეს არის int (integer) მთელი რიცხვი
+height = 155.6 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool)
+
+knows_programming = True #True ან False
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
+
+print("my name is" + " " + name + " " + surname + " " + "my age is" + " " + str(age) + " " + "my height is" + " " + str(height))
