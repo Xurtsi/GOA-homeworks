1567bc612b1ea4702b5238e1485e13b318c4920e
Xurtsi <mariamxurcilava363@gmail.com>
2025-03-14 06:54:32 -0700

    commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,31 @@
+# 0)
+x = 4
+y = 44
+z = 10
+print(x)
+print(y)
+print(z)
+
+# 1)
+a = 2
+b = 8
+print(a+b)
+
+# 2)
+string1 = "5"
+string2 = "10"
+print(string1 + string2)
+
+
+# 3)
+c = 6
+print(c)
+c = 4
+
+
+
+# 4)
+number = "5"
+# print("Result: " + numb)
+
+print("Result: " + number)
