7615bb8ff8b184cbd7c605cedc3dcbf482118e1f
Xurtsi <mariamxurcilava363@gmail.com>
2025-03-22 12:07:09 -0700

    commit

---
+++ day 06/classwork/classwork01.py
@@ -0,0 +1,32 @@
+# 1
+#age = input("please input your age: ")
+#print(age)
+
+# 2
+#name = input("please input your name: ")
+#print("Hello " + name)
+
+#name = "14"
+#age = 13
+#Height = 155.6
+#stugyinginGOA
+
+#print(type(name)) #<class "str">
+#print(type(age)) #<class "int">
+#print(type(Height)) #<class "float">
+#print(type(stugyinginGOA)) #<class "bool">
+#print(type(name)) #<class "str">
+#print(type(age)) #<class "int">
+#print(type(Height)) #<class "float">
+#print(type(stugyinginGOA)) #<class "bool">
+
+#age = 13
+#print("Hello you are " + str(age) + " years old")
+
+#num1 = input("please input number 1: ")
+#num2 = input("please input number 2: ")
+#print(int(num1) + int(num2))
+#print(int(num1) - int(num2))
+#print(int(num1) * int(num2))
+#print(int(num1) / int(num2))
+
---
+++ day 06/homework/homework.py
@@ -0,0 +1,21 @@
+# 1)
+num1 = input("please input number: ")
+print(int(num1))
+
+
+# 2)
+Playingvolleyball = input("do you play volleyball?; ") 
+print(type(Playingvolleyball)) #<class "str">
+
+
+# 3)
+num = "15"
+print(int(num))
+
+
+# 4)
+# type() ფუნქცია ამოწმებს მონაცემის ტიპს
+
+
+# 5)
+# input() ფუნქციას შემოაქვს ინფორმაცია string-ში
