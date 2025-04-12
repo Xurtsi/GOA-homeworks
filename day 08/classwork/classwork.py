be7dc8ffd789a881c30bbc4369546d4f3525221a
Xurtsi <mariamxurcilava363@gmail.com>
2025-04-12 07:49:39 -0700

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,39 @@
+# 0) 
+print(15 > 25)
+print(15 < 25)
+print(15 >= 25)
+print(15 <= 25)
+print(15 == 25)
+
+# 1)
+user_num1 = int(input("Enter number: "))
+user_num2 = int(input("Enter number: "))
+print(user_num1 + user_num2)
+print(user_num1 - user_num2)
+print(user_num1 * user_num2)
+print(user_num1 / user_num2)
+
+
+# 2)
+# True
+print(20 > 10)
+print(20 < 21)
+print(20 >= 10)
+print(20 <= 21)
+print(20 == 20)
+
+# False
+print(20 > 21)
+print(20 < 10)
+print(20 >= 21)
+print(20 <= 10)
+print(20 == 10)
+
+
+# 3)
+user_num3 = int(input("Enter number: "))
+print(user_num3 > 10)
+
+
+# 4)
+✅
