5728cecbbceed1566f003d3c61b2d70bf5af6723
Xurtsi <mariamxurcilava363@gmail.com>
2025-04-27 03:20:52 -0700

    commit

---
+++ day 10/classwork/classwork01.py
@@ -0,0 +1,16 @@
+# 1)
+print(True and False or False and False or True or True or False)
+# ეს კოდი გამოიტანს True-ს. პირველი სრულდება "and" შემდეგ "or".
+# True and False = False
+# False and False = False
+# False and False = False
+# False or True = True
+# True or True = True
+# True or False = True
+
+
+# 2)
+# ლოგიკური ოპერაციების დროს პირველი სრულდება "and"
+
+
+# 3) "and" ჰგავს "1. * ან /" მათემატიკურ ოპერაციას
---
+++ day 10/homework/homework.py
@@ -0,0 +1,15 @@
+# 1) ✅
+
+
+# 2)
+print(True and not False and (False or True) and True or not False)
+#ეს კოდი გამოიტანს True-ს იმიტომ რომ; 
+False or True = False 
+#True and not False = True
+#True and False = True
+# True and True = true
+#True or not False = True
+
+# 3) ✅
+
+# 4) ✅
