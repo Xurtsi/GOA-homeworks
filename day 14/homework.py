932872b9a39e39d4e65067171a40ba127d76f9a6
Xurtsi <mariamxurcilava363@gmail.com>
2025-05-30 07:55:02 -0700

    commit

---
+++ day 14/homework.py
@@ -0,0 +1,30 @@
+# 1. გაიხსენეთ და კომენტარებში დაწერეთ ყველა ნასწავლი შედარების ოპერაცია
+# 2. გაიხსენეთ და კომეტარებში დაწერეთ ყველა ლოგიკური ოპერაცია
+# 3. ერთ კოდში გამოიყენეთ ორივე ლოგიკური და შედარებითი ოპერაცია
+# 4.  sololearnში დაასრულეთ module 3ის quiz
+# 5. კომენტარებით დაწერეთ რას გამოიტანს ეს კოდი:
+# print(True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False))
+
+
+# 1.შედარების ოპერაციები:  < ; >= ;  <= ; == 
+
+# 2. ლოგიკური ოპერაციები: and ; or ; not
+
+# 3. 
+
+# 4. ✅
+
+# 5. print(True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False))
+# ეს კოდი გამოიტანს - True
+# (False or True) - True
+# (True or False) - True
+# (True and False) - False
+# True and not False - True
+# True and True - True
+# True and True - True
+# True or not False - True
+# True and False - False
+# False and not True - False
+# False and True - False
+# False or False - False
+# True or False - True
